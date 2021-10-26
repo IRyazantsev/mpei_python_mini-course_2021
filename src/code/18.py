6a43b7ef24c452d8495c046a2e2907ba21372d96
IRiazantsev <riazantsev@bl-g.ru>
2021-10-26 13:22:02 +0300

    fix

--- src/code/18.py
+++ src/code/18.py
@@ -1,13 +1,15 @@
 leds = [('LED1', 40, 6000),
         ('LED2', 60, 9000),
         ('LED3', 90, 12000),
-        ('LED4', 80, 12000),]
+        ('LED4', 80, 12000), ]
 
-led_max = None
+led_selected = None
 for led in leds:
-    if led_max == None:
-        led_max = led
-    elif led[2] > led_max[2]:
-        if led[1] < led_max[2]:
-            led_max = led
-print(led_max[0])
+    if led_selected == None:
+        led_selected = led
+    elif led[2] > led_selected[2]:
+        led_selected = led
+    elif led[2] == led_selected[2]:
+        if led[1] < led_selected[1]:
+            led_selected = led
+print(led_selected[0])
